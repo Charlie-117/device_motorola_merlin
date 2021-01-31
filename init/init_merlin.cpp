8eb9f8c5821b9f9399bd73ce3f886e5ce94f18ea
Tony Jose <gmoto1179@gmail.com>
2021-01-31 15:31:48 +0530

    merlin: Bring back custom init
    This reverts commit cc5a85e40edf2f4d24c3e8aa6638ba42f7f34543,
    And also adapts it for R.
    
    [Ref:LineageOS/android_device_motorola_victara]
    
    Change-Id: Iaec84695ca731db7ed1ffc3bf003f5e4bfbb58e1

---
+++ init/init_merlin.cpp
@@ -0,0 +1,106 @@
+/*
+   Copyright (c) 2013, The Linux Foundation. All rights reserved.
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are
+   met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of The Linux Foundation nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <vector>
+
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+
+#include <android-base/properties.h>
+#include <android-base/logging.h>
+
+#include "property_service.h"
+#include "vendor_init.h"
+
+using android::base::GetProperty;
+using android::init::property_set;
+
+std::vector<std::string> ro_props_default_source_order = {
+    "",
+    "odm.",
+    "product.",
+    "system.",
+    "vendor.",
+};
+
+void property_override(char const prop[], char const value[], bool add = true)
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else if (add)
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+void vendor_load_properties()
+{
+    std::string sku;
+    std::string radio;
+    std::string device;
+
+    radio = GetProperty("ro.boot.radio", "");
+    sku = GetProperty("ro.boot.hardware.sku", "");
+
+    const auto set_ro_build_prop = [](const std::string &source,
+            const std::string &prop, const std::string &value) {
+        auto prop_name = "ro." + source + "build." + prop;
+        property_override(prop_name.c_str(), value.c_str(), false);
+    };
+
+    const auto set_ro_product_prop = [](const std::string &source,
+            const std::string &prop, const std::string &value) {
+        auto prop_name = "ro.product." + source + prop;
+        property_override(prop_name.c_str(), value.c_str(), false);
+    };
+
+    if (sku == "XT1556" || radio == "0x6") {
+        /* XT1556 - Mexico, Colombia, Brazil*/
+        property_override("ro.build.description", "merlin_retail-user 6.0.1 MPD24.107-56 30 release-keys");
+        property_set("persist.radio.process_sups_ind", "1");
+        property_set("ro.gsm.data_retry_config", "default_randomization=2000,max_retries=infinite,1000,1000,80000,125000,485000,905000");
+        property_set("ro.mot.build.customerid", "retail");
+        for (const auto &source : ro_props_default_source_order) {
+            set_ro_build_prop(source, "fingerprint", "motorola/merlin_retail/merlin:6.0.1/MPD24.107-56/30:user/release-keys");
+            set_ro_product_prop(source, "device", "merlin");
+            set_ro_build_prop(source, "product", "merlin");
+        }
+    } else if (sku == "XT1557" || radio == "0x9") {
+        /* XT1557 - India */
+        property_override("ro.build.description", "merlin_retasia-user 6.0.1 MPD24.107-56 30 release-keys");
+        property_set("ro.mot.build.customerid", "retasia");
+        for (const auto &source : ro_props_default_source_order) {
+            set_ro_build_prop(source, "fingerprint", "motorola/merlin_retasia/merlin:6.0.1/MPD24.107-56/30:user/release-keys");
+            set_ro_product_prop(source, "device", "merlin");
+            set_ro_build_prop(source, "product", "merlin");
+        }
+    }
+
+    device = GetProperty("ro.product.device", "");
+    LOG(ERROR) << "Found sku '" << sku.c_str() << "' setting build properties for '" << device.c_str() << "' device\n";
+}
