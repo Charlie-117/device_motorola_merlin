dd6a07ca308433b7b0fd1e03d6fde2f8201e07cf
Tony Jose <gmoto1179@gmail.com>
2021-05-11 16:11:24 +0530

    merlin: Back to Lineage

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 Android Ice Cold Project
+# Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,8 +14,7 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aicp_merlin.mk
+    $(LOCAL_DIR)/lineage_merlin.mk
 
 COMMON_LUNCH_CHOICES := \
-    aicp_merlin-userdebug \
-    aicp_merlin-eng
+    lineage_merlin-userdebug
